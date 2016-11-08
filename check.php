f4560bc017c73b16363a4ad4e9530bb176313d82
eXteVlad <extevlad@yandex.ru>
2016-11-08 17:23:11 +0400

    Авторизация/регистрация
    Авторизация/регистрация пашет. Профиль пустой пока

---
+++ check.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+	header( 'Content-Type: text/html; charset=utf-8' );
+	if(isset($_POST['login_btn']))
+	{
+		$login = $_POST['login'];
+		$pass = $_POST['password'];
+		$mysqli = new mysqli('localhost', 'root', '', 'WeddingBD');
+		if(!$mysqli)
+		{
+			printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error()); 
+			exit; 
+		}
+		$query = $mysqli->query("SELECT Login, Password FROM users WHERE Login = '$login' AND password = '$pass'");
+		if($query->num_rows == 1)
+		{
+			$_SESSION['login'] = $login;
+			header('location:profile.php');
+			exit;
+		}
+		else
+		{
+			$_SESSION['wrong_login'] = 1;
+			header('location:index.php');
+			exit;
+		}
+		$mysqli->close();
+	}
+	else if(isset($_POST['reg_btn']))
+	{
+		$login = $_POST['login'];
+		$pass = $_POST['password'];
+		$email = $_POST['email'];
+		$username = $_POST['username'];
+		$date = $_POST['date'];
+		$mysqli = new mysqli('localhost', 'root', '', 'WeddingBD');
+		if(!$mysqli)
+		{
+			printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error()); 
+			exit; 
+		}
+		$qrchecklogin = $mysqli->query("SELECT * FROM users WHERE login = '$login' LIMIT 1");
+		$qrcheckemail = $mysqli->query("SELECT * FROM users WHERE Email = '$email' LIMIT 1");
+		if($qrchecklogin->num_rows == 1)
+		{
+			$_SESSION['busyname'] = 1;
+			header('location:registration.php');
+			exit;
+		}
+		else if($qrcheckemail->num_rows == 1)
+		{
+			$_SESSION['busyemail'] = 1;
+			header('location:registration.php');
+			exit;
+		}
+		else
+		{
+			if($mysqli->query("INSERT INTO users (Login, Password, Email, UserName, Marriage_Date) VALUES ('$login', '$pass', '$email', '$username', '$date')"))
+			{
+				$_SESSION['regsuc'] = 1;
+				header('location:index.php');
+				exit;
+			}
+			else 
+			{
+				$err = $mysqli->error;
+				echo $err;
+			}
+		}
+	}
+?>
---
+++ registration.php
@@ -0,0 +1,51 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<link rel="shortcut icon" href="img/favicon.ico">	
+		<link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css">
+		<link rel = "stylesheet" type = "text/css" href = "../css/registration.css">
+		<title>Регистрация</title>
+	</head>
+	<body>
+		<div id = "main">
+			<center>
+				<h1>Регистрация</h1>
+				<h2>Введите необходимые данные в поля для регистрации.</h2>
+				<form action = "check.php" method = "post">
+					<p>Логин:</p>
+					  <input name = "login" type="text" id="inputLogin" placeholder="Логин">
+					  <?php
+						if(isset($_SESSION['busyname']))
+						{
+							echo "<p style = 'color: red'>Такой логин уже занят.</p>";
+							unset($_SESSION['busyname']);
+							session_destroy();
+						}
+					  ?>
+					<p>Пароль:</p>
+					 <input name = "password" type="password" id="inputPassword" placeholder="Пароль">
+					<p>E-mail:</p>
+					  <input name = "email" type="text" id="inputEmail" placeholder="Email">
+					  <?php
+						if(isset($_SESSION['busyemail']))
+						{
+							echo "<p style = 'color: red'>Такой E-mail уже занят.</p>";
+							unset($_SESSION['busyemail']);
+							session_destroy();
+						}
+					  ?>
+					<p>Имя:</p>
+					  <input name = "username" type="text" id="inputLogin" placeholder="Имя">
+					<p>Дата свадьбы:</p>
+					  <input name = "date" type="date" id="inputDate">
+					  </br>
+					  <button name = "reg_btn" type="submit" class="btn btn-primary">Создать</button>
+				</form>
+				<a href = "index.php"><button class = "btn btn-warning">Назад</button></a>
+			</center>
+		</div>
+	</body>
+</html>
